f8cd6f05c2ffb5beb1c8869cb69d177c42ce690c
Andreas Waldenburger <andreas@waldenburger.name>
2014-07-15 02:06:17 +0200

    Factored stuff out into a Tag class.
    (... to prepare for unittests ...)

--- note.py
+++ note.py
@@ -17,37 +17,67 @@ def get_arguments():
 
     return parser.parse_args()
 
+class Tag:
+    tagdef = re.compile(r'(?<!\\|:)//(.+?)//', flags=re.M|re.S)
+    """This is what a //tag definition// looks like.
+
+    Can be escaped by either prepending the first slashes with
+    either ``\`` or a ``:`` (so URIs like ``http://...``
+    don't become tag definitions).
+    """
+
+    spaces = re.compile(r'\s+')
+
+    @classmethod
+    def make_tags(Cls, path):
+        """Extract ``Tag``'s from file at ``path``.
+
+        :path: string that contains tag definitions
+        :returns: set of ``Tag``'s
+        """
+        tags = set()
+        with open(path) as f:
+            txt = f.read()
+            matches = Cls.tagdef.finditer(txt) or []
+            for match in matches:
+                namestring = match.group(1)
+                fullmatch = match.group(0)
+                tag = Cls(namestring, path, fullmatch)
+                tags.add(tag)
+        return tags
+
+    def __init__(self, name, path, address):
+        """Create a tag.
+
+        :name: the tag string (can contain spaces)
+        :path: path to the file the tag is defined in
+        :address: ``ex``-command to find the definition of the tag
+        """
+        # Collapse spaces (including newlines)
+        self.name = name.strip()
+        self.name = self.spaces.sub(' ', self.name)
+
+        # The file path. We leave it alone.
+        self.path = path
+
+        # Replace some characters that have special meaning to the ex search 
+        # pattern, so we get a 'literal' string. TODO: Do this properly, for 
+        # all special characters; this is bound to blow up with some weird tag 
+        # string or another.
+        address = address
+        address = address.replace('\n', r'\n')
+        address = address.replace('/', r'\/')
+        self.address = '/' + address + '/'
+
+    def __str__(self):
+        return '{}\t{}\t{}\n'.format(self.name, self.path, self.address)
+
 def cmd_mktags():
-    class rgx:
-        spaces = re.compile(r'\s+')
-        tag = re.compile(r'(?<!\\|:)//(.+?)//', flags=re.M|re.S)
-        # Every string //like this// is a tag, but only if the start of the 
-        # pattern isn't prefixed with a '\' (to generically escape it) or a ':' 
-        # (because URIs may have a "http://..." part).
-
-    def tagname(string):
-        """Collapse spaces"""
-        txt = string.strip()
-        txt = rgx.spaces.sub(' ', txt)
-        return txt
-
-    tagline = '{}\t{}\t{}\n'
-
     tags = set()
     for filename in iglob('*.txt'):
-        with open(filename) as f:
-            txt = f.read()
-            matches = rgx.tag.finditer(txt) or []
-            for match in matches:
-                s = match.group(1)
-                address = match.group(0)
-                # Replace newline character with the string '\n',
-                # so it works in the search pattern.
-                address = address.replace('\n', r'\n')
-                address = address.replace('/', r'\/')
-                address = '/' + address + '/'
-                tag = tagline.format(tagname(s), filename, address)
-                tags.add(tag)
+        filetags = [str(t) for t in Tag.make_tags(filename)]
+        tags = tags.union(filetags)
+        print(tags)
 
     with open('tags', 'w') as tagfile:
         tagfile.writelines(sorted(tags))
