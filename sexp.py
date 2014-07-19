c3f6686dd3120c2fa65e28b7ee50a1df7b831327
Andreas Waldenburger <andreas@waldenburger.name>
2014-07-19 23:36:39 +0200

    Got a combination function working; still needs polish.

--- sexp.py
+++ sexp.py
@@ -20,3 +20,42 @@ def break_list(string):
     assert stack[-1] is result
     return result
 
+def make_groups(string):
+    from collections import defaultdict
+    from itertools import product
+    import pdb
+
+    parts = re.split(parens, string)
+
+    ### Break the sequence into groups
+    groupnumber = 0
+    groupstack = [groupnumber]
+    groups = defaultdict(list)
+    for n, part in enumerate(parts):
+        if part == '(':
+            groupnumber += 1
+            groupstack.append(groupnumber)
+        elif part == ')':
+            groupstack.pop()
+        else:
+            current = groupstack[-1]
+            groups[current].append(n)
+    print(groups)
+    ### Assemble all permutations of the groups
+    out = []
+    assert len(groups) == groupnumber+1
+    combinations = product((True, False), repeat=groupnumber)
+    for combination in combinations:
+        # Gather all indices
+        selected = set(groups[0])
+        for n in range(groupnumber):
+            if combination[n]:
+                selected.update(groups[n+1])
+        # Combine all selected parts
+        val = "".join(parts[n]
+            for n in range(len(parts))
+            if n in selected)
+        out.append(val)
+    return out
+
+g = make_groups(s)
